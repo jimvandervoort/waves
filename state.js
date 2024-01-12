46b73681845b2c4ca58ccf98b513224e56635375
Jim van der Voort <jim@jim.computer>
2024-01-12 14:18:40 +0200

    Replace control with some state utils

---
+++ state.js
@@ -0,0 +1,58 @@
+export const makeState = () => {
+	const storage = {};
+	let updateCb;
+
+	const handler = {
+		set(obj, prop, value) {
+			const result = Reflect.set(...arguments);
+			if (updateCb) updateCb();
+			return result;
+		},
+	};
+
+	const state = new Proxy(storage, handler);
+	const onUpdate = (newUpdateCb) => {
+		updateCb = newUpdateCb;
+	}
+
+	return {
+		state,
+		onUpdate,
+	}
+}
+
+export const attachInput = (state, prop, selector) => {
+	const input = document.querySelector(selector);
+	state[prop] = parseFloat(input.value);
+
+	input.addEventListener('input', (e) => {
+		state[prop] = parseFloat(e.target.value);
+	});
+}
+
+export const attachKeyToggle = (state, prop, key, initialValue) => {
+	const stored = localStorage.getItem(prop);
+	if (stored === null) {
+		state[prop] = initialValue;
+	} else {
+		state[prop] = stored === "true";
+	}
+
+	document.addEventListener('keypress', (e) => {
+		if (e.key === key) {
+			state[prop] = !state[prop];
+		}
+
+		localStorage.setItem(prop, `${state[prop]}`);
+	});
+}
+
+export const attachClickToggle = (state, prop, elem) => {
+	if (typeof elem === 'string') {
+		elem = document.querySelector(elem);
+	}
+
+	elem.addEventListener('click', () => {
+		state[prop] = !state[prop];
+	});
+}
